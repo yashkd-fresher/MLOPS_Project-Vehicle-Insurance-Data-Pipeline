5e7e77bf90ce3facbf4660582560d1184f83f979
Yash <yashdeshmane2003@gmail.com>
2026-05-17 21:31:51 +1000

    logging and exception added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="yash deshmane",
+    author_email="deyash966@gmail.com",
     packages=find_packages()
 )
