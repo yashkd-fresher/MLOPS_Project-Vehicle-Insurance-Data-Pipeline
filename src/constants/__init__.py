f8854c84bd9e76b43c4b8ba6bb9f862acb198750
Yash <yashdeshmane2003@gmail.com>
2026-05-18 21:51:54 +1000

    app ready locally

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -72,4 +72,4 @@ MODEL_PUSHER_S3_KEY = "model-registry"
 
 
 APP_HOST = "0.0.0.0"
-APP_PORT = 5000
+APP_PORT = 8080
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -51,7 +51,7 @@ class ModelTrainerConfig:
     _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
     _criterion = MIN_SAMPLES_SPLIT_CRITERION
     _random_state = MIN_SAMPLES_SPLIT_RANDOM_STATE
- 
+
 @dataclass
 class ModelEvaluationConfig:
     changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
@@ -61,4 +61,9 @@ class ModelEvaluationConfig:
 @dataclass
 class ModelPusherConfig:
     bucket_name: str = MODEL_BUCKET_NAME
-    s3_model_key_path: str = MODEL_FILE_NAME    
+    s3_model_key_path: str = MODEL_FILE_NAME
+
+@dataclass
+class VehiclePredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
