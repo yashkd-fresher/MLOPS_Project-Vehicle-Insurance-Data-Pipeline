20fa0d2bfdc9b14a0464d136534ecd57a7117aa4
Yash <yashdeshmane2003@gmail.com>
2026-05-18 12:19:16 +1000

    all set for data injestion

--- demo.py
+++ demo.py
@@ -10,12 +10,17 @@
 # --------------------------------------------------------------------------------
 
 # # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
